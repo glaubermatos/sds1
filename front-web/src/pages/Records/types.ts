db92e1c36bedabe6e5522698010bbf94be48b2d8
glaubermatos <glaub.oliveira@hotmail.com>
2020-09-19 16:47:04 -0300

    Criando pagina de listagem dos registros

---
+++ front-web/src/pages/Records/types.ts
@@ -0,0 +1,16 @@
+export type RecordsResponse = {
+    content: RecordItem[];
+    totalPages: number;
+}
+
+export type RecordItem = {
+    age: number;
+    gamePlatform: Platform;
+    gameTitle: string;
+    genreName: string;
+    id: number;
+    moment: string;
+    name: string;
+}
+
+export type Platform = 'XBOX' | 'PC' | 'PLAYSTATION';
