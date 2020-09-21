3d5aece6a3f9ed516be3554306d001d370b337a3
glaubermatos <glaub.oliveira@hotmail.com>
2020-09-20 23:36:05 -0300

    Configurando front-web para produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -7,7 +7,7 @@ import Pagination from './Pagination';
 import './styles.css';
 import Filters from '../../components/Filters';
  
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://sds1-glauber.herokuapp.com";
 
 const Records = () => {
 
